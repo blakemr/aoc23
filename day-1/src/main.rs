a0d31de107816ce681de8b966e74246a11dd2bdf
Matthew <matthewblake0625@gmail.com>
2023-12-01 09:18:19 -0500

    reset the cache.

---
+++ day-1/src/main.rs
@@ -0,0 +1,145 @@
+use std::{fs::File, io::Read};
+
+fn main() {
+    let mut text = String::new();
+    File::open("input.txt")
+        .expect("Failed to open file.")
+        .read_to_string(&mut text)
+        .expect("Failed to read file.");
+
+    dbg!(part_1(text.as_str()));
+    dbg!(part_2(text.as_str()));
+}
+
+fn part_1(input: &str) -> u32 {
+    let mut sum = 0;
+    for line in input.lines() {
+        let first = line
+            .chars()
+            .find(|c| c.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
+        let last = line
+            .chars()
+            .rfind(|c| c.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
+
+        sum += (first * 10) + last;
+    }
+
+    sum
+}
+
+fn part_2(input: &str) -> u32 {
+    let mut sum = 0;
+    for mut line in input.lines() {
+        let mut first = [
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine",
+        ]
+        .iter()
+        .find(|first| line.starts_with(*first));
+
+        while first.is_none() && !line.is_empty() {
+            line = &line[1..];
+            first = [
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine",
+            ]
+            .iter()
+            .find(|first| line.starts_with(*first));
+        }
+
+        let first = first.unwrap();
+
+        let first = first
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+
+        let first = first
+            .chars()
+            .find(|c| c.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
+
+        let mut last = [
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine",
+        ]
+        .iter()
+        .find(|last| line.ends_with(*last));
+
+        while last.is_none() && !line.is_empty() {
+            line = &line[..line.len() - 1];
+            last = [
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine",
+            ]
+            .iter()
+            .find(|last| line.ends_with(*last));
+        }
+
+        let last = last.unwrap();
+
+        let last = last
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+
+        let last = last
+            .chars()
+            .rfind(|c| c.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
+
+        sum += (first * 10) + last;
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1() {
+        let test_input = "1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet";
+
+        assert_eq!(part_1(test_input), 142);
+    }
+
+    #[test]
+    fn p2() {
+        let test_input = "two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen";
+
+        assert_eq!(part_2(test_input), 281);
+    }
+}
