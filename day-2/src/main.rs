158de943fef53c357ba72ff552f99c2410e06ba8
Matthew <matthewblake0625@gmail.com>
2023-12-02 09:07:35 -0500

    day 2 done

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,108 @@
+use std::{fs::File, io::Read};
+
+struct Bag {
+    idx: u32,
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Bag {
+    fn new(input: &str) -> Self {
+        let (idx, games) = input.split_once(':').unwrap();
+
+        let (_, idx) = idx
+            .split_once("Game ")
+            .unwrap_or_else(|| panic!("Failed to split \"{idx}\" from: {idx}, {games}"));
+        let idx: u32 = idx.parse().unwrap();
+
+        let games = games.split(';');
+        let (mut red, mut green, mut blue) = (0, 0, 0);
+        for game in games {
+            let colors = game.split(',');
+            for color in colors {
+                let (n, c) = color.trim().split_once(' ').unwrap();
+                match c {
+                    "red" => red = red.max(n.parse().unwrap()),
+                    "green" => green = green.max(n.parse().unwrap()),
+                    "blue" => blue = blue.max(n.parse().unwrap()),
+                    s => panic!("Unknown input: {}", s),
+                }
+            }
+        }
+
+        Bag {
+            idx,
+            red,
+            green,
+            blue,
+        }
+    }
+
+    fn can_hold(&self, r: u8, g: u8, b: u8) -> bool {
+        self.red <= r && self.green <= g && self.blue <= b
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut text = String::new();
+    File::open("input.txt")
+        .expect("Failed to open file.")
+        .read_to_string(&mut text)
+        .expect("Failed to read file.");
+
+    dbg!(part_1(text.as_str()));
+    dbg!(part_2(text.as_str()));
+}
+
+fn part_1(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let b = Bag::new(line);
+        if b.can_hold(12, 13, 14) {
+            sum += b.idx;
+        }
+    }
+
+    sum
+}
+
+fn part_2(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let b = Bag::new(line);
+        let power: u32 = b.red as u32 * b.blue as u32 * b.green as u32;
+        sum += power;
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1() {
+        let test_input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+        assert_eq!(part_1(test_input), 8);
+    }
+
+    #[test]
+    fn p2() {
+        let test_input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+        assert_eq!(part_2(test_input), 2286);
+    }
 }
